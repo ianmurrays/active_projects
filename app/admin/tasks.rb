35dcd24827aca9a53eb9f5b9192e8069de6c263b
Ian Murray <ianmurrays@gmail.com>
2011-09-10 15:10:07 -0300

    Adds tasks control panel tweaks

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,3 +1,33 @@
 ActiveAdmin.register Task do
+  scope :all, :default => true
+  scope :due_this_week do |tasks|
+    tasks.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now)
+  end
+  scope :late do |tasks|
+    tasks.where('due_date < ?', Time.now)
+  end
+  scope :mine do |tasks|
+    tasks.where(:admin_user_id => current_admin_user.id)
+  end
   
+  show do
+    panel "Task Details" do
+      attributes_table_for task do
+        row("Status") { status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+        row("Title") { task.title }
+        row("Project") { link_to task.project.title, admin_project_path(task.project) }
+        row("Assigned To") { link_to task.admin_user.email, admin_admin_user_path(task.admin_user) }
+        row("Due Date") { task.due_date? ? l(task.due_date, :format => :long) : '-' } 
+      end
+    end
+    
+    active_admin_comments
+  end
+  
+  sidebar "Other Tasks For This User", :only => :show do
+    table_for current_admin_user.tasks.where(:project_id => task.project) do |t|
+      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+    end
+  end
 end
