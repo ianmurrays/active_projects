7b0f5ed067cdde20c7957125fe2ce381b2b2ebd4
Ian Murray <ianmurrays@gmail.com>
2011-09-10 12:11:08 -0300

    Rails 3.1 and Active Admin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveProjects::Application.config.secret_token = '74850a3913a8f8e31c3514363bf9f2fcc7154310f002a07b5340301763c37c842c40d815de812879a73b5b45babc152db2f987c601730cda663e2c8c1693b501'
