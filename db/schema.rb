d5e5230f9c5a7681f281ff126316cd7027dc6e8e
Ian Murray <ianmurrays@gmail.com>
2011-09-10 13:12:05 -0300

    Adds projects admin interface

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110910151139) do
+ActiveRecord::Schema.define(:version => 20110910155730) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -45,5 +45,11 @@ ActiveRecord::Schema.define(:version => 20110910151139) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "projects", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
