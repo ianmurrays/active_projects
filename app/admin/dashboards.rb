53c9237b8aab62521be9299c320044299347220c
Ian Murray <ianmurrays@gmail.com>
2011-09-10 15:10:10 -0300

    Adds dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,4 +1,22 @@
 ActiveAdmin::Dashboards.build do
+  
+  section "Your tasks for this week" do
+    table_for current_admin_user.tasks.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now) do |t|
+      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+      t.column("Assigned To") { |task| task.admin_user.email }
+      t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
+    end
+  end
+  
+  section "Tasks that are late" do
+    table_for current_admin_user.tasks.where('due_date < ?', Time.now) do |t|
+      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+      t.column("Assigned To") { |task| task.admin_user.email }
+      t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
+    end
+  end
 
   # Define your dashboard sections here. Each block will be
   # rendered on the dashboard in the context of the view. So just
